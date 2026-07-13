d08c2f2232355346291d18f373e6b44cbe89e5c4
Javier <35864946+jmagit@users.noreply.github.com>
2026-07-13 14:34:59 +0200

    personalización

--- curso/src/app/app.ts
+++ curso/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('curso');
+  protected readonly title = signal('world');
 }
